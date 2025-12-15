1b75f26528917fc13b917bfdc5256b04a9ae09bc
ThomasLORIOT <thomas.loriot@utt.fr>
2025-12-15 15:14:42 +0100

    feat: add dbt sources and staging model for uber trips

---
+++ models/staging/stg_uber_trips.sql
@@ -0,0 +1,25 @@
+{{ config(materialized='table') }}
+
+select
+    VendorID as vendor_id,
+    tpep_pickup_datetime as pickup_datetime,
+    tpep_dropoff_datetime as dropoff_datetime,
+    passenger_count,
+    trip_distance,
+    RatecodeID as rate_code_id,
+    PULocationID as pickup_location_id,
+    DOLocationID as dropoff_location_id,
+    payment_type,
+    fare_amount,
+    extra,
+    mta_tax,
+    tip_amount,
+    tolls_amount,
+    improvement_surcharge,
+    total_amount,
+    congestion_surcharge,
+    airport_fee
+from {{ source('raw', 'yellow_tripdata_2023_01') }}
+where pickup_datetime is not null
+  and dropoff_datetime is not null
+  and total_amount > 0
